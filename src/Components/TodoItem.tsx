63b9fbe350e2d498459822ba1b36cdbeb0a66895
Bogdan Kalytovskiy <bogdankalytovskkiy@gmail.com>
2025-08-10 14:34:14 +0300

    add sol

--- src/Components/TodoItem.tsx
+++ src/Components/TodoItem.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
+
 import { Todo } from '../types/Todo';
 
 interface Props {
